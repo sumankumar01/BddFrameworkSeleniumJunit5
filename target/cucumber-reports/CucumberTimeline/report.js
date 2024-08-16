8090a4d2220988664c01059b89f29adc47f875ec
suman <suman.giri007@gmail.com>
2024-08-16 19:18:42 +0530

    remove target

--- target/cucumber-reports/CucumberTimeline/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"84d72f46-06d8-4681-85b1-3c0b6b1f3844","feature":"login to saucedemo application","scenario":"login with valid user","start":1723807405243,"group":1,"content":"","tags":"","end":1723807405476,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
-});
--- target/test-classes/hellocucumber/loginSuaceLab.feature
+++
@@ -1,6 +0,0 @@
-Feature: login to saucedemo application
-
-  Scenario: login with valid user
-    Given I am on login page
-    When I am entering the <userName> and <passWord>
-    Then Home page should display
